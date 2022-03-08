158215ac327c35a79527cba1ecda44c25180baf1
FernandoZap <fernando9az@gmail.com>
2022-03-07 21:05:20 -0300

    acrescentando user no admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
 
-from django.contrib.auth import admin as auth_admin
-
-from .models import User
-
-admin.site.register(User, auth_admin.UserAdmin)
+# Register your models here.
