7dcd56939ea4fdef1c3600085778b1e34ed14090
FernandoZap <fernando9az@gmail.com>
2022-03-22 16:10:24 -0300

    processando 2000 linhas

--- app01/importarPlanilha.py
+++ app01/importarPlanilha.py
@@ -424,7 +424,7 @@ def importarSecFuncVincEventos(planilha,id_municipio,anomes,current_user,municip
     lista_eventos=listagens.listagemEventos(id_municipio)
 
 
-    while row<sheet.max_row+1 and row<2000:
+    while row<sheet.max_row+1 and row<1000:
 
         secretaria = sheet['W' + str(row)].value # DC 
         setor = sheet['X' + str(row)].value # DC 
