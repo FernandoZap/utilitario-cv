38ab1c4337b70d32ace34f64c1d496e01c37a542
FernandoZap <fernando9az@gmail.com>
2022-03-22 10:54:08 -0300

    configurando para ate 50 linhas da planilha

--- app01/importarPlanilha.py
+++ app01/importarPlanilha.py
@@ -393,7 +393,7 @@ def importarSecFuncVincEventos(planilha,id_municipio,anomes,current_user,municip
     lista_eventos=listagens.listagemEventos(id_municipio)
 
 
-    while row<sheet.max_row+1 and row<6500:
+    while row<sheet.max_row+1 and row<50:
 
         secretaria = sheet['W' + str(row)].value # DC 
         setor = sheet['X' + str(row)].value # DC 
