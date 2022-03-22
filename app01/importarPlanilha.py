592730cdcc10b234906fb1630b4ea63418a2565e
FernandoZap <fernando9az@gmail.com>
2022-03-22 17:49:16 -0300

    configurando timeout para 300

--- app01/importarPlanilha.py
+++ app01/importarPlanilha.py
@@ -578,7 +578,7 @@ def importarSetores(planilha,id_municipio,anomes,current_user,municipio,mes_ref)
     dict_secretarias=listagens.criarDictSecretarias(id_municipio)
     lista_secretarias = listagens.listagemSecretarias(id_municipio)
 
-    while row<sheet.max_row+1 and row<6000:
+    while row<sheet.max_row+1 and row<700:
 
         secretaria = sheet['W' + str(row)].value # DC 
         setor = sheet['X' + str(row)].value # DC
