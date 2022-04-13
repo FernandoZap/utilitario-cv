9764df3653334b5fe3c5ce417db4cd6f863fae8e
FernandoZap <fernando9az@gmail.com>
2022-04-12 21:43:37 -0300

    acertando a planilha excel

--- app01/importarPlanilha.py
+++ app01/importarPlanilha.py
@@ -535,7 +535,6 @@ def importarSecFuncVincEventos(i_id_municipio,i_anomes,entidade,empresa):
                         evento_new.save()
         ls_evento_verificado.append(evento)
 
-        '''
         if funcao is not None:
             funcao=funcao.strip()
             if len(funcao)>2:
@@ -563,7 +562,6 @@ def importarSecFuncVincEventos(i_id_municipio,i_anomes,entidade,empresa):
 
 
         ls_funcao_verificada.append(funcao)                            .
-        '''
 
 
     if len(ls_secretaria)>0:
