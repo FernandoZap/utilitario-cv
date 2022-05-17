0ab99a56190b325586f168fcac1405569294a75e
FernandoZap <fernando9az@gmail.com>
2022-05-17 07:33:04 -0300

    criando imprimirFolhaLayout 13

--- app01/views.py
+++ app01/views.py
@@ -987,7 +987,7 @@ def imprimirFolhaLayout(request):
                         listaEventosDoServidor=[]
                     for qq in range(len(eventos)):
                         if eventos[qq] in listaEventosDoServidor:
-                            valor=dicionario[eventos[qq]]
+                            valor=0 #dicionario[eventos[qq]]
                             #soma+=valor
                             valor_str=str(valor)
                             valor_str = valor_str.replace('.',',')
